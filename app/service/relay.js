8c13d48321f34a68e57c906c8db98d5fc38c0cb1
aguestuser <aguestuser@riseup.net>
2018-10-06 22:34:35 -0400

    cleanup error-logging for failed message transmission

--- app/service/relay.js
+++ app/service/relay.js
@@ -3,8 +3,8 @@ const exec = util.promisify(require('child_process').exec)
 
 const relay =(msg, recipients) =>
   exec(`signal-cli --dbus-system send -m "${msg}" ${fmtRecipients(recipients)}`)
-    .then(() => `Relayed message "${msg}" to [${recipients}]`)
-    .catch(console.error)
+    .then(() => `--- SUCCESS: Relayed message "${msg}" to [${recipients}]`)
+    .catch(err => console.error(`--- TRANSMISSION ERROR: ${err}`))
 
 const fmtRecipients = recipients =>
   recipients.map(r => `"${r}"`).join(" ")
