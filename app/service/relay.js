b750bd47766d1282b46fa57a75055283b509d2f8
aguestuser <aguestuser@riseup.net>
2018-11-19 13:54:46 -0800

    placeholder: use `dbus-send` to call into dbus w/o JVM spinup

--- app/service/relay.js
+++ app/service/relay.js
@@ -1,8 +1,12 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
+// const service = "dbus-org.asamk.Signal.service"
+const destination = "org.asamk.Signal"
+
+//TODO: use dbus-native instead of calling shell command
 const relay =(msg, recipients) =>
-  exec(`signal-cli --dbus-system send -m "${msg}" ${fmtRecipients(recipients)}`)
+  exec(`dbus-send --system --type=method_call --print-reply --dest="org.asamk.Signal" /org/asamk/Signal org.asamk.Signal.sendMessage string:'${msg}' array:string: string:'${recipients[0]}'`)
     .then(() => `--- SUCCESS: Relayed message "${msg}" to [${recipients}]`)
     .catch(err => console.error(`--- TRANSMISSION ERROR: ${err}`))
 
@@ -10,4 +14,3 @@ const fmtRecipients = recipients =>
   recipients.map(r => `"${r}"`).join(" ")
 
 module.exports = relay
-
