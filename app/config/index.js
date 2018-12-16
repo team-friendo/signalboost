033a775b99e0b5265c7f553d9d3bff1fea3b6dc2
aguestuser <aguestuser@riseup.net>
2018-12-15 22:25:08 -0800

    [21] rename RELAY_PHONE_NUMBER -> CHANNEL_PHONE_NUMBER

--- app/config/index.js
+++ app/config/index.js
@@ -4,5 +4,6 @@ const dbConfigsByEnv = require('./db.json')
 const getConfig = cfg => get(cfg, [process.env.NODE_ENV || 'production'])
 
 module.exports = {
-  db: getConfig(dbConfigsByEnv)
+  db: getConfig(dbConfigsByEnv),
+  channelPhoneNumber: process.env.CHANNEL_PHONE_NUMBER,
 }
