c365b7eb2f6e8f698a8f9514814b9b4e45e097f5
aguestuser <aguestuser@riseup.net>
2019-01-11 16:53:10 -0500

    [19|bugfix] prevent early returns by using `await` in authenticator

--- app/service/api/middleware/authenticator.js
+++ app/service/api/middleware/authenticator.js
@@ -1,12 +1,16 @@
 const {
   api: { authToken },
+  twilio: { smsEndpoint },
 } = require('../../../config')
 
-const configureAuthenticator = app =>
-  app.use(async function authenticator(ctx, next) {
-    const { token } = ctx.request.headers
-    token === authToken ? next() : respondNotAuthorized(ctx)
-  })
+const configureAuthenticator = app => app.use(authenticator)
+
+const authenticator = async (ctx, next) =>
+  isAuthorized(ctx) ? await next() : respondNotAuthorized(ctx)
+
+// TODO(aguestuser): only allow twilio to make requests to this url... how??
+const isAuthorized = ctx =>
+  ctx.path === `/${smsEndpoint}` || ctx.request.headers.token === authToken
 
 const respondNotAuthorized = ctx => {
   ctx.status = 401
