36d6deb43659838c27e6a680b62d8a5d4ccecf35
aguestuser <aguestuser@riseup.net>
2019-04-15 16:35:56 -0400

    [42] send welcome message to new admin when added by existing admin
    * refactor `messenger#dispatch` to call `parseMessageType`
    * introduce a `NOTIFICATION` type (here use it for notifying newly
      added admins, but can extend it for other notifications later)
    * introduce a handling layer between dispatch and message-sending
      functions to route messages based on incoming message type
    * when welcome message notification is parsed (for newly added admin):
      * respond to the sender as before
      * also send welcome message to the admin who was just  added
      * add `messenger#notify` and `messages.notifications.welcome` to do
        the latter

--- app/services/dispatcher/messenger.js
+++ app/services/dispatcher/messenger.js
@@ -1,18 +1,69 @@
+const { isEmpty } = require('lodash')
 const signal = require('./signal')
 const messages = require('./messages')
 const { commands, statuses } = require('./executor')
 const messageCountRepository = require('../../db/repositories/messageCount')
 
+/**
+ * type MessageType = 'BROADCAST' | 'RESPONSE' | 'NOTIFICATION'
+ */
+
+const messageTypes = {
+  BROADCAST: 'BROADCAST',
+  RESPONSE: 'RESPONSE',
+  NOTIFICATION: 'NOTIFICATION',
+}
+
+/***************
+ * DISPATCHING
+ ***************/
+
 // (CommandResult, Dispatchable) -> Promise<void>
 const dispatch = async ({ commandResult, dispatchable }) => {
-  const { command, status } = commandResult
-  const isBroadcastMessage = status === statuses.NOOP
-  return isBroadcastMessage
-    ? dispatchable.sender.isAdmin
-      ? broadcast(dispatchable)
-      : respond({ ...dispatchable, message: messages.unauthorized, status: statuses.UNAUTHORIZED })
-    : respond({ ...dispatchable, message: commandResult.message, command, status })
+  const messageType = parseMessageType(commandResult)
+  return {
+    [messageTypes.BROADCAST]: () => handleBroadcast(dispatchable),
+    [messageTypes.RESPONSE]: () => handleResponse({ commandResult, dispatchable }),
+    [messageTypes.NOTIFICATION]: () => handleNotification({ commandResult, dispatchable }),
+  }[messageType]()
 }
+
+// CommandResult -> MessageType
+const parseMessageType = commandResult => {
+  if (commandResult.status === statuses.NOOP) return messageTypes.BROADCAST
+  else if (isNotifiable(commandResult)) return messageTypes.NOTIFICATION
+  else return messageTypes.RESPONSE
+}
+
+const isNotifiable = ({ command, status }) =>
+  command === commands.ADD && status === statuses.SUCCESS
+
+const handleBroadcast = dispatchable =>
+  dispatchable.sender.isAdmin
+    ? broadcast(dispatchable)
+    : respond({ ...dispatchable, message: messages.unauthorized, status: statuses.UNAUTHORIZED })
+
+const handleResponse = ({ commandResult, dispatchable }) => {
+  const { message, command, status } = commandResult
+  return respond({ ...dispatchable, message, command, status })
+}
+
+const handleNotification = ({ dispatchable, commandResult }) => {
+  const { command, status, message, payload } = commandResult
+  const { channel, sender } = dispatchable
+  return Promise.all([
+    respond({ ...dispatchable, message, command, status }),
+    notify({
+      ...dispatchable,
+      message: messages.notifications.welcome(channel, sender.phoneNumber),
+      recipients: [payload],
+    }),
+  ])
+}
+
+/************
+ * SENDING
+ ************/
 
 // Dispatchable -> Promise<void>
 const broadcast = async ({ db, iface, channel, message, attachments }) => {
@@ -29,6 +80,11 @@ const respond = ({ db, iface, channel, message, sender, command, status }) => {
     .sendMessage(iface, formattedMessage, [sender.phoneNumber])
     .then(() => countCommand({ db, channel }))
 }
+
+const notify = ({ db, iface, channel, message, recipients }) =>
+  signal
+    .sendMessage(iface, format(channel, message), recipients)
+    .then(() => countCommand({ db, channel }))
 
 // string -> string
 const format = (channel, message, command, status) =>
@@ -48,4 +104,4 @@ const countCommand = ({ db, channel }) =>
   // TODO(@zig): add prometheus counter increment here
   messageCountRepository.incrementCommandCount(db, channel.phoneNumber)
 
-module.exports = { messages, dispatch, broadcast, respond, format }
+module.exports = { messageTypes, parseMessageType, dispatch, broadcast, respond, format }
