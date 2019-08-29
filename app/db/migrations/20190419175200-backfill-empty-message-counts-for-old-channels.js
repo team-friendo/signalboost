f204000ce4d7fb98471c45940f869fec693d1823
aguestuser <aguestuser@riseup.net>
2019-08-29 17:06:09 -0400

    [89] add migration for responsesEnabled field on channel table
    side-effect: remove backfill migration (should not be a migration anyway!)

--- app/db/migrations/20190419175200-backfill-empty-message-counts-for-old-channels.js
+++ app/db/migrations/20190419175200-backfill-empty-message-counts-for-old-channels.js
@@ -4,33 +4,33 @@ const db = initDb()
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const numbersWithCounts = await db.messageCount.findAll().map(mc => mc.channelPhoneNumber)
-
-    const numbersWithoutCounts = await db.channel
-      .findAll({
-        where: {
-          phoneNumber: {
-            [Op.notIn]: numbersWithCounts,
-          },
-        },
-      })
-      .map(ch => ch.phoneNumber)
-
-    if (numbersWithoutCounts.length !== 0) {
-      console.log(`creating message counts for ${numbersWithoutCounts}...`)
-
-      await db.messageCount.bulkCreate(
-        numbersWithoutCounts.map(channelPhoneNumber => ({
-          channelPhoneNumber,
-          broadcastIn: 0,
-          broadcastOut: 0,
-          commandIn: 0,
-          commandOut: 0,
-        })),
-      )
-      console.log(`created message counts!`)
-    } else {
-      console.log('no message counts needed or created.')
-    }
+    // const numbersWithCounts = await db.messageCount.findAll().map(mc => mc.channelPhoneNumber)
+    //
+    // const numbersWithoutCounts = await db.channel
+    //   .findAll({
+    //     where: {
+    //       phoneNumber: {
+    //         [Op.notIn]: numbersWithCounts,
+    //       },
+    //     },
+    //   })
+    //   .map(ch => ch.phoneNumber)
+    //
+    // if (numbersWithoutCounts.length !== 0) {
+    //   console.log(`creating message counts for ${numbersWithoutCounts}...`)
+    //
+    //   await db.messageCount.bulkCreate(
+    //     numbersWithoutCounts.map(channelPhoneNumber => ({
+    //       channelPhoneNumber,
+    //       broadcastIn: 0,
+    //       broadcastOut: 0,
+    //       commandIn: 0,
+    //       commandOut: 0,
+    //     })),
+    //   )
+    //   console.log(`created message counts!`)
+    // } else {
+      // console.log('no message counts needed or created.')
+    // }
   },
 }
