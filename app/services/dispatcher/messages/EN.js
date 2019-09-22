468ace323ba1149798f45c7f2ce16a4a7c3f67e4
aguestuser <aguestuser@riseup.net>
2019-09-22 19:42:33 -0400

    [100] add HELP message about REAUTHORIZE

--- app/services/dispatcher/messages/EN.js
+++ app/services/dispatcher/messages/EN.js
@@ -70,6 +70,10 @@ ADD +1-555-555-5555
 REMOVE +1-555-555-5555
 -> removes +1-555-555-5555 as an admin
 
+REAUTHORIZE +1-555-555-5555
+-> restores admin rights to +1-555-555-5555 after they have re-installed signal
+-> (admin rights are taken away when an admin reinstalls signal to protect from an attacker impersonating the admin)
+
 GOODBYE / ADIOS
 -> removes you from channel`,
     subscriber: `
@@ -83,7 +87,9 @@ HELLO / HOLA
 -> subscribes you to messages
 
 GOODBYE / ADIOS
--> unsubscribes you`,
+-> unsubscribes you
+
+`,
   },
 
   // INFO
