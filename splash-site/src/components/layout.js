922d2f711c99b9d4203a365e20d1cbf29e713d24
aguestuser <aguestuser@riseup.net>
2019-11-04 17:50:24 -0800

    [101] add gatsby, draft copy for index

---
+++ splash-site/src/components/layout.js
@@ -0,0 +1,39 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header siteTitle='Signalboost' />
+      <div
+        style={{
+          margin: `8rem auto 0 auto`, // TRBL
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ splash-site/src/pages/index.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Signalboost: Secure Textblasts and Hotlines for Activists" />
+    <p>
+      Signalboost lets activists send text blasts and receive hotline tips
+      on their phones without revealing their identity or spending money. It is <em>secure, simple, and free.</em>
+    </p>
+    <h3>Secure:</h3>
+    <ul>
+      <li>
+        Signalboost sends messages over {' '}
+        <Link to="signal.org">Signal</Link>, the most secure encrypted
+        messaging service available for phones.
+      </li>
+      <li>
+        It does not display phone numbers or names. People can send and receive
+        messages without sharing their identity -- and feel safer if anyone's
+        phone is stolen.
+      </li>
+      <li>
+        Signalboost retains the minimal user metadata necessary to route
+        messages. Its maintainers, Team Friendo, will resist any attempt to
+        compel us disclose it, and are working on{' '}
+        <Link to="0xacab.org/team-friendo/signalboost/issues/68">updates</Link>{' '}
+        make such disclosure impossible.
+      </li>
+    </ul>
+    <h3>Simple:</h3>
+    <ul>
+      <li>
+        Signalboost is for 1-way messaging only. Admins can send announcements
+        to large groups and receive hotline tips from anyone. That's it.
+      </li>
+      <li>
+        It does not allow subscribers to send messages to each other. This cuts
+        out cross talk endemic to large Signal or WhatsApp groups.
+      </li>
+      <li>
+        It tries to solve one problem and solve it well. This makes it easier to
+        know what it's for amidst a dizzying sea of tech tools!
+      </li>
+    </ul>
+    <h3>Free:</h3>
+    <ul>
+      <li>
+        We are making this software for people's collective liberation, not for
+        profit. We do not charge money for it and never will
+      </li>
+    </ul>
+
+    <h1>Getting Started:</h1>
+    <h3>To get a Signalboost channel:</h3>
+    <ul>
+      <li>Send a Signal message to +1-938-444-8536</li>
+      <li>
+        Include a name for the channel and the phone numbers of all admins
+      </li>
+      <li>
+        You will receive a welcome message as soon as your channel is created
+      </li>
+    </ul>
+
+    <h3>Once you have a channel:</h3>
+    <ul>
+      <li>
+        There will be a signal phone number associated with your channel.
+        Anytime you send a message to it, anyone who is subscribed will get that
+        message.
+      </li>
+      <li>
+        People can subscribe to announcements by sending a Signal message to
+        this phone number that says "HELLO" or "HOLA" and leave by sending
+        "GOOBYE" or "ADÌOS."
+      </li>
+      <li>
+        You can convert the channel into a hotline by sending a message that
+        says "RESPONSES ON" to your channel number.
+      </li>
+    </ul>
+
+    <h1>Learn More:</h1>
+    <p>If you have questions, we'd love to hear from you! You can...</p>
+    <ul>
+      <li>Send us a signal message at +1-938-444-8536</li>
+      <li>Send us an email at team-friendo@protonmail.com</li>
+      <li>
+        Visit our&nbsp;
+        <Link to="0xacab.org/team-friendo/signalboost">gitlab page</Link>
+        &nbsp;to learn more technical details, read our source code, file a bug
+        report, or become a contributor!
+      </li>
+    </ul>
+  </Layout>
+)
+
+export default IndexPage
