8e7127fe0a9d96e2f9b7793aa2b55ff0bd8ad0df
fdbk <fdbk@riseup.net>
2020-02-13 14:47:16 -0500

    [106] Change fn signatures in common PN service

--- app/services/registrar/phoneNumber/common.js
+++ app/services/registrar/phoneNumber/common.js
@@ -31,21 +31,21 @@ const errorStatus = (error, phoneNumber) => ({
 const extractStatus = phoneNumberInstance =>
   pick(phoneNumberInstance, ['status', 'phoneNumber', 'twilioSid'])
 
-// (Database, Socket, Channel, String) -> Channel
+// (Database, Socket, Channel, String, String) -> Promise<void>
 const notifyMembersExcept = async (db, sock, channel, message, sender) => {
   if (channel == null) return
   const memberPhoneNumbers = channelRepository.getMemberPhoneNumbersExcept(channel, [sender])
   await signal.broadcastMessage(sock, memberPhoneNumbers, signal.sdMessageOf(channel, message))
 }
 
-// Channel -> Promise<void>
+// (DB, Socket, String) -> Promise<void>
 const notifyMaintainers = async (db, sock, message) => {
   const adminChannel = await channelRepository.findDeep(db, signupPhoneNumber)
   const adminPhoneNumbers = channelRepository.getAdminPhoneNumbers(adminChannel)
   await signal.broadcastMessage(sock, adminPhoneNumbers, signal.sdMessageOf(adminChannel, message))
 }
 
-// Channel -> Promise<void>
+// (DB, Socket, ChannelInstance, String) -> Promise<void>
 const destroyChannel = async (db, sock, channel, message) => {
   if (channel == null) return
   try {
@@ -56,6 +56,7 @@ const destroyChannel = async (db, sock, channel, message) => {
   }
 }
 
+// () -> TwilioInstance
 const getTwilioClient = () => require('twilio')(accountSid, authToken)
 
 module.exports = {
