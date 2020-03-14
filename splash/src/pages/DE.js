413820d1d81ef1f641ab4b37ca1b796bac3ce264
divinetta <divinette@riseup.net>
2020-03-14 19:03:40 +0100

    rebase on 206 how to guide splash

--- splash/src/pages/DE.js
+++ splash/src/pages/DE.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
@@ -108,83 +106,6 @@ const IndexPage = () => (
         &nbsp;um mehr technische Details oder den Quellcode nachzulesen, neue Funktionen vorzuschlagen, Fehler zu melden oder mitzuhelfen!
       </li>
     </ul>
-    <h1>FAQ</h1>
-    <ul>
-    	<li>
-			<a href="#why">Warum sollte ich Signalboost benutzen?</a><br/>
-		</li>
-		<li>
-			<a href="#security">Wie schützt Signalboost meine Telefonnummer und Identität?</a>
-		</li>
-		
-		<li>
-			<a href="#secure">Wie sichert Signalboost meine Nachrichten?</a>
-		</li>
-		<li>
-			<a href="#signal">Brauche ich Signal um einen Signalboost Kanal nutzen zu können?</a>
-		</li>
-		<li>
-			<a href="#costl">Was kostet es Signalboost zu nutzen?</a>
-		</li>
-		<li>
-			<a href="#vouche">Können wir kontrollieren wer sich für unseren Kanal anmeldet?</a>
-		</li>
-		<li>
-			<a href="#opensource">Wer ist die Eigentümer*in von Signalboost? Ist Signalboost open-source? Wer hat euch bezahlt Signalboost zu machen?</a>
-		</li>
-		<li>
-			<a href="#listshare">Kann ich eine Liste der Teilnehmer*innen auf meinem Signalboost Kanal einsehen?</a> 
-		</li>
-		<li>
-			<a href="#listshare">Kann ich einen Signalboost Kanal auf dem teamfriendo server bekommen?</a> 
-		</li>	    			
-	</ul>
-    
-    <h3><span name="why" class="anchor"></span>Warum sollte ich Signalboost benutzen?</h3>
-    <p>
-    	Signalboost solltest du benutzen wenn du die hohen Kosten durch SMS Verteiler vermeiden möchtest und du oder deine Community bereits die sichere Nachrichtenapp Signal nutzt. 					Mit Signalboost kannst du auf Signal zurückgreifen um Nachrichten an große Gruppen anderer Signalnutzer*innen zu senden ohne dabei ihre Telefonnummern (die oft eine sehr persönliche und identifizierbare Information ist) allen anderen Gruppenteilnehmer*innen preiszugeben.				Prinzipiell ist Signalboost wie eine Sms-Schleuder die über Signal gesichert ist.
-    </p>
-    <h3><span name="security" class="anchor"></span>Wie schützt Signalboost meine Telefonnummer und Identität?</h3>
-    <p>
-    	Signalboost schützt deine Identität indem es deine Telefonnummer vor anderen Teilnehmer*innen des Kanals verbirgt. Selbst Admins können niemals die Telefonnummern der Kanal-Teilnehmer*innen einsehen. Nur der Signalboost Server und seine Maintainer*innen haben Zugriff auf die Nummern der Teilnehmer*innen der einzelnen Kanäle es ist also sehr wichtig, dass sie sich dazu verpflichten diese Informationen zu beschützen.
- 
-    </p>
-    <h3><span name="secure" class="anchor"></span>Wie sichert Signalboost meine Nachrichten? </h3>
-    <p>
-    	Genau wie dein Telefon und alle mit denen du über Signal kommunizierst, nutzt Signalboost Verschlüsselung um sicherzustellen dass nur die gewünschten Empfänger*innen die Nachricht lesen können. Da der Signalboostserver jedoch in der Praxis ein Empfänger ist, muss er die Nachricht entschlüsseln um sie dann an deinen Kanal weitergeben zu können. Sollte ein Signalboostserver kompromitiert werden könnten Nachrichten die er entschlüsselt also genau so wie bei einem kompromitierten Telefon ausgelesen werden. Im Gegensatz zu jedem Telefon behält Signalboost aber keine Kopie der Nachrichten die es sendet oder empfängt. Die Nachrichten werden, sobald sie weitergeleitet wurden gelöscht, egal was passiert.
-	</p>
-    <h3><span name="signal" class="anchor"></span>Brauche ich Signal um einen Signalboost Kanal nutzen zu können?</h3>
-    <p>
-    	Ja. Alle Teilnehmer*innen an einem Signalboost Kanal müssen Signal benutzen. Signal stellt sowohl die Verschlüsselung, als auch den Auslieferungsmechanismus für deine Signalboost Nachrichten.
-
-    <h3><span name="cost" class="anchor"></span>Was kostet es Signalboost zu nutzen?</h3>
-    <p>
-    	Signalboost ist freie Software und der Signalboost Server von teamfriendo ist frei zur Benutzung durch Communities und Oraganisationen die wir unterstützen wollen.
-Es steht allen frei einen Signalboost Server zu hosten und andere Maintainer könnten dafür Gebühren verlangen. Im Gegensatz zu vielen anderen Messaging Apps sind mit dem Versenden von Nachrichten keine Kosten verbunden, da es zum Nachrichtenversand auf Signal zurückfreift statt auf kostspielige SMS-Netzwerke.
-    </p>
-    <h3><span name="vouche" class="anchor"></span>Können wir kontrollieren wer sich für unseren Kanal anmeldet?</h3>
-	<p>
-		Ja! Signalboost hat einen "Vertrauen" Modus. Ist dieser aktiviert braucht es entweder eine Einladung eines Admins oder einer anderen Teilnehmer*in um einem Kanal beitreten zu können.
-	</p>
-	<h3><span name="opensource" class="anchor"></span>Wer ist die Eigentümer*in von Signalboost? Ist Signalboost open-source? Wer hat euch bezahlt Signalboost zu machen?</h3> 
-	<p>
-		Signalboost ist open-source und steht zur freien Benutzung bereit. Einjeder.m mit dem Können und Willen einen eigenen Signalboost Server mit unserer Software aufzusetzen steht es frei das zu tun. Signalboost wurde zum größten Teil ohne direkte Finanzierung geschrieben, aus Liebe.<br />
-		<a href="https://0xacab.org/team-friendo/signalboost">Hier kannst du den Signalboost Quellcode einsehen und lernen wie du eine eigene Instanz aufsetzen kannst &#8680;</a>
-	</p>
-	<h3><span name="listshare" class="anchor"></span>Kann ich eine Liste der Teilnehmer*innen auf meinem Signalboost Kanal einsehen?</h3>
-	<p>
-		Nein. Das ist ein grundlegendes Feature von Signalboost. Die Nummern von Teilnehmer*innen sind weder von anderen Teilnehmer*innen noch von Admins des Kanals einsehbar. Nur Signalboost Server Maintainer können diese Daten einsehen, ihnen must du also vertrauen. Im Falle von teamfriendo verbietet es unsere Datenschutzrichtlinie diese Daten jemals preiszugeben oder mit irgendwem zu teilen. 
- 
-	</p>
-	<h3><span name="listshare" class="anchor"></span>Can I pay you to add a feature to Signalboost?</h3>
-	<p>
-		Vielleicht. Wenn wir es als eine sinnvolle funktionale Erweiterung für die Gruppen denen wir Helfen wollen ansehen, sind wir einem zusätzlichen Anreiz gegenüber nicht abgeneigt. Du kannst den Quellcode auch abspalten und hinzufügen was du möchtest (solange das Resultat Open Source bleibt) oder bei uns mitwirken. <br />
-		<a href="mailto:team-friendo@riseup.net">Schick uns eine Email, wenn du mehr über eine Idee sprechen möchtest &#8680;</a>
-	</p>
-	<h3><span name="teamfriendo" class="anchor"></span>Kann ich einen Signalboost Kanal auf dem teamfriendo server bekommen?</h3>
-	<p>
-		Signalboost ist in einem limitierten beta Lauf und in aktiver Entwicklung. Zur Zeit stellen wir keine Kanäle auf unserer Instanz öffentlich zur Verfügung. Wenn du gerne zu unserem eingeschränkten beta Programm hinzustoßen möchtest kannst du gerne versuchen uns eine <a href="mailto:team-friendo@riseup.net">eMail</a> mit deinen Bedürfnissen zu schicken. Es kann gut sein, dass wir dir nicht antworten da unser Fokus auf der Entwicklung liegt damit wir bald aus dem beta Lauf raus kommen! 
-	</p>
   </Layout>
 )
 
--- splash/src/pages/DEhow-to.js
+++ splash/src/pages/DEhow-to.js
@@ -5,56 +5,91 @@ import { Link } from 'gatsby'
 export default () => (
   <Layout>
     <p>
-      Diese Seite richtet sich primär an Admins die bereits einen Signalboost Kanal haben. Wenn du einen Kanal brauchst schau mal in den{' '} <Link to="/">Getting Started</Link> Abschnitt. Wenn du mehr über Signalboost und darüber wie sicher die Benutzung ist erfahlen möchtest, wirf einen blick ins{' '}
+      Diese Seite richtet sich primär an Admins die bereits einen Signalboost Kanal haben. Wenn du einen Kanal brauchst schau mal in den{' '} <Link to="/">Getting Started</Link> Abschnitt. Wenn du mehr über Signalboost und darüber wie sicher die Benutzung ist erfahlen möchtest, wirf einen Blick ins{' '}
       <Link to="/faq">FAQ's.</Link>
     </p>
-    <h3>Conceptual overview</h3>
+    <h3>Konzept Übersicht</h3>
     <p>
-      Ein Signalboost Kanal hat Admins und Teilnehmer*innen. Wenn ein Admin eine Nachricht an den Kanal schickt, wird diese entweder als Befehl interpretiert oder als Meldung. Ist die Nachricht eine Meldung, wird sie an alle Teilnehmer*innen weitergeleitet - wobei als Absender stets die Signalnummer des Kanals angezeigt wird, nicht dem Admin der sie geschickt hat.
+	Ein Signalboost Kanal hat Admins und Teilnehmer*innen.
+	 Wenn ein Admin eine Nachricht an den Kanal schickt, wird diese entweder als Befehl interpretiert oder als Meldung.
+	 Ist die Nachricht eine Meldung, wird sie an alle Teilnehmer*innen weitergeleitet - wobei als Absender stets die Signalnummer des Kanals angezeigt wird, nicht dem Admin der sie geschickt hat.
     </p>
+<h3>Was ist ein Befehl?</h3>
+    <p>
+      Ein Befehl ist ein Wort oder eine Phrase die Signalboost als Anweisung interpretiert. Um einen Befehl zu nutzen musst du garnichts kompliziertes tun - schreibe ihn einfach in den Kanal und Signalboost wird ihn interpretieren oder eine Fehlermeldung ausgeben! Einige Befehle können nur von Admins ausgeführt werden. Wenn du dir nicht sicher bist welcher Befehl der Richtige ist, dann ist HILFE immer ein guter Anfang.
+    </p>
+    <p>
+      Signalboost unterstützt zur Zeit Englisch, Spanisch, Französich und Deutsch. Um in deine Wahlsprache zu wechseln kannst du einfach den Namen der Sprache in den Kanal tippen. Wenn ich zum Beispiel meine Spracheinstellung auf Spanisch umstellen möchte, würde ich "ESPAÑOL" an den Kanal senden.
+    </p>
+
+    <h2>Verschwindende Nachrichten</h2>
+    <p>
+      Von Haus aus verschwinden die Nachrichten auf allen Signalboost Kanälen nach einer Woche. Aber Admins (und nur Admins) können über den Timer für verschwindende Nachrichten in der oberen rechten Ecke der Signal app, die 1-Wochen Periode umstellen.
+    </p>
+
     <h2>Admin Befehle</h2>
     <h4>HILFE</h4>
-    <p>-> Zeigt alle Befehle an</p>
+    <p>-> Zeigt alle Befehle die du nutzen kannst an</p>
 
     <h4>INFO</h4>
-    <p>-> Zeigt einige statistische Infos über den Kanal, und erklärt wie Signalboost funktioniert</p>
-
-    <h4>UMBENENNEN neuer name</h4>
-    <p>-> Benennt den Kanal in "neuer name" um</p>
-    <p>Beispiel:</p>
-
-    <h4>BESCHREIBUNG beschreibung des kanals</h4>
-    <p>-> Fügt eine öffentliche Beschreibung des Kanals hinzu oder erneuert diese</p>
-
-    <h4>EINLADEN +491701234567</h4>
-    <p>-> Lädt +491701234567 ein sich beim Kanal anzumelden</p>
+    <p>-> Zeigt einige statistische Infos über den Kanal, und erklärt in Kurzform wie Signalboost funktioniert</p>
 
     <h4>HINZUFÜGEN / ENTFERNEN +491701234567</h4>
-    <p>-> Fügt +491701234567 hinzu, oder entfernt diese als Admin des Kanals</p>
+    <p>-> Fügt +491701234567 hinzu, oder entfernt diese als Admin des Kanals. Jede.r Admin kann andere als Admin hinzufügen oder entfernen</p>
+
+    <h4>EINLADEN +491701234567</h4>
+    <p>-> Lädt +491701234567 ein sich beim Kanal anzumelden. Nicht vergessen die Telefonnummer mit einem + und dem Ländercode zu beginnen</p>
 
     <h4>HOTLINE AN / AUS</h4>
-    <p>-> Schaltet die Hotline Funktion an oder aus </p>
-
-    <h4>VERTRAUEN AN / AUS</h4>
-    <p>-> Bestimmt ob es einer Einladung bedarf um sich beim Kanal anzumelden</p>
-
-    <h4>VERTRAUENS-LEVEL level</h4>
-    <p>-> Verändert die Zahl der benötigten Einladungen um dem Kanal beitreten zu können</p>
+    <p>-> Schaltet die Hotline Funktion an oder aus. Das bedeutet, dass Teilnehmer*innen dem Kanal schreiben können und die Nachricht anonym den Admins weitergeleitet wird. In der Standarteinstellung starten Signalboost Kanäle mit ausgeschalteter Hotline Funktion. Wenn du ein Admin bist, kannst du einer Nachricht ansehen, dass sie eine Hotline Nachrciht ist weil sie mit folgender Kopfzeile beginnt:</p>
+  <p>
+      <b>[HOTLINE MESSAGE]</b>
+    </p>
+    <p>
+      Wenn die Hotline eingeschaltet wird: 
+	<ul>
+	<li>Bleibt die Anonymität der Teilnehmer*innen gewahrt.</li>
+	<li>
+	  Das bedeutet, dass Teilnehmer*innen ihre Nummer in die Nachricht schreiben müssen wenn sie in Kontakt mit einem der Admins treten wollen.
+	</li>
+	<li>
+	  Jenachdem wie viele Personen am Kanal teilnehmen, kann die Hotline sehr gesprächig sein und erhöht die Warscheinlichkeit für Spam und Missbrauch
+	</li>
+      </ul>
+    </p>
 
     <h4>ESPAÑOL / FRANÇAIS / ENLISH</h4>
-    <p>-> Stellt die Sprache auf Spanisch, Französisch oder Englisch um</p>
+    <p>-> Stellt die Sprache auf Spanisch, Französisch oder Englisch um. Spracheinstellungen für Signalboost sind Nutzerspezifisch du musst dir damit also keine Sorgen um andere machen. 
+    </p>
+
+    <h4>UMBENENNEN neuer name</h4>
+    <p>
+	Benennt den Kanal in "neuer name" um</p>
+    <p>Beispiel: UMBENENNEN Mein Kooler Signalboost Kanal</p>
+
+    <h4>BESCHREIBUNG beschreibung des kanals</h4>
+    <p>
+	Fügt eine öffentliche Beschreibung des Kanals hinzu oder erneuert diese</p>
+
+    <h4>VERTRAUEN AN / AUS</h4>
+    <p>
+	Wenn Vertrauen angeschaltet ist können nur Personen die eine Einladung über den EINLADEN Befehl erhalten haben dem Kanal als Teilnehmer*in beitreten. Einladungen bleiben gültig auch wenn Vertrauen mehrfach an- und ausgeschaltet wird
+    </p>
+
+    <h4>VERTRAUENS-LEVEL level</h4>
+    <p>
+	Verändert die Zahl der benötigten Einladungen um dem Kanal beitreten zu können; zur Zeit kann das auf eine Zahl zwischen 1 und 10 gestellt werden.
+    </p>
+
 
     <h4>TSCHÜSS</h4>
-    <p>-> Entfernt dich aus diesem Kanal</p>
+    <p>
+	Entfernt dich aus diesem Kanal. Falls du ein Admin bist verlierst du damit den Admin Zugang zum Kanal kannst aber weiter als Teilnehmer*in beitreten. Möchtest du wieder als Admin hinzugefügt werden, musst du eine.n existenten Admin darum bitten dich mit dem HINZUFÜGEN Befehl als Admin hinzuzufügen.
+    </p>
 
     <h4>VERNICHTEN</h4>
-    <p>-> Löscht den Kanal und alle zugehörigen Daten unwiderruflich</p>
-
-    <h2>Teilnehmer*innen Befehle</h2>
     <p>
-      Teilnehmer*innen können fast alle Befehle nutzen die auch Admins zur verfügung stehen, ausser jenen die das Verhalten des Kanals verändern
-
+	VORSICHT! Dieser Befehl löscht den Kanal und alle zugehörigen Daten unwiderruflich. Achtung, der Nachrichtenverlauf auf den Admin und Teilnehmer*innen Telefonen bleibt bis der Timer für verschwindende Nachrichten abgelaufen ist erhalten!
     </p>
   </Layout>
 )
-
