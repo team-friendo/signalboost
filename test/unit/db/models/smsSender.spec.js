b6945fd9759ab3d290b3f6a5a790d669caf3020d
aguestuser <aguestuser@riseup.net>
2020-05-25 17:03:23 -0400

    [105] add `smsSender` model

---
+++ test/unit/db/models/smsSender.spec.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai'
+import { describe, it, before, after, afterEach } from 'mocha'
+import { initDb } from '../../../../app/db/index'
+import { smsSenderFactory } from '../../../support/factories/smsSender'
+
+describe('smsSender model', () => {
+  let db
+
+  before(() => (db = initDb()))
+  afterEach(() => db.smsSender.destroy({ where: {}, force: true }))
+  after(() => db.sequelize.close())
+
+  it('has correct fields', async () => {
+    const smsSender = await db.smsSender.create(smsSenderFactory())
+
+    expect(smsSender.phoneNumber).to.be.a('string')
+    expect(smsSender.messagesSent).to.be.a('number')
+    expect(smsSender.createdAt).to.be.a('Date')
+    expect(smsSender.updatedAt).to.be.a('Date')
+  })
+
+  it('sets correct defaults', async () => {
+    const smsSender = await db.smsSender.create(
+      smsSenderFactory({
+        messagesSent: undefined,
+        createdAt: undefined,
+        updatedAt: undefined,
+      }),
+    )
+    expect(smsSender.messagesSent).to.eql(0)
+    expect(smsSender.createdAt).to.be.a('Date')
+    expect(smsSender.updatedAt).to.be.a('Date')
+  })
+
+  describe('validations', () => {
+    it('does not allow a null phone number', async () => {
+      const err = await db.smsSender
+        .create(smsSenderFactory({ phoneNumber: undefined }))
+        .catch(e => e)
+      expect(err.message).to.include('phoneNumber cannot be null')
+    })
+  })
+})
