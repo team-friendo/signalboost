7c0f7e350de6bdd40785db285bba53cf6b6e6093
aguestuser <aguestuser@riseup.net>
2020-06-01 19:42:30 -0400

    [96] add hotlineMessageRepository
    it has 3 methods:
    
    * `findOrCreateId`: retrieves or creates a `hotlineMessage` record for a given
      channel + member combination, returns the id of the record (to use
      as "message number")
    * `findMemberPhoneNumber`: retrieves the member phone number
      corresponding to a given hotlineMessage id (or reject a promise if
      that hotline message record has expired)
    * `deleteExpired`: deletes all `hotlineMessage` records older than 1
      month (to protect member metadata)

--- test/support/factories/hotlineMessages.js
+++ test/support/factories/hotlineMessages.js
@@ -1,8 +1,6 @@
-import { random } from 'lodash'
 import { genPhoneNumber } from './phoneNumber'
 
 export const hotlineMessageFactory = attrs => ({
-  id: random(0, 1000000),
   channelPhoneNumber: genPhoneNumber(),
   memberPhoneNumber: genPhoneNumber(),
   ...attrs,
