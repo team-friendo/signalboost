56501643229640d1aa1e493e1c3e872ca9478204
aguestuser <aguestuser@riseup.net>
2020-06-27 10:03:23 -0400

    [hotfix] remove rate limit notification

--- app/dispatcher/index.js
+++ app/dispatcher/index.js
@@ -1,6 +1,6 @@
 const signal = require('../signal/signal')
 const callbacks = require('../signal/callbacks')
-const { sdMessageOf, messageTypes } = signal
+const { messageTypes } = signal
 const channelRepository = require('../db/repositories/channel')
 const membershipRepository = require('../db/repositories/membership')
 const { memberTypes } = membershipRepository
@@ -12,10 +12,7 @@ const safetyNumberService = require('../registrar/safetyNumbers')
 const { messagesIn } = require('./strings/messages')
 const { get, isEmpty, isNumber } = require('lodash')
 const metrics = require('../metrics')
-const {
-  signal: { supportPhoneNumber },
-  defaultLanguage,
-} = require('../config')
+const { defaultLanguage } = require('../config')
 
 /**
  * type Dispatchable = {
@@ -75,11 +72,14 @@ const dispatch = async msg => {
   if (rateLimitedMessage) {
     const resendInterval = resend.enqueueResend(rateLimitedMessage)
     logger.log(
-      messagesIn(defaultLanguage).notifications.rateLimitOccurred(channel.name, resendInterval),
+      messagesIn(defaultLanguage).notifications.rateLimitOccurred(
+        channelPhoneNumber,
+        resendInterval,
+      ),
     )
     metrics.incrementCounter(metrics.counters.ERRORS, [
       metrics.errorTypes.RATE_LIMIT,
-      channel.phoneNumber,
+      channelPhoneNumber,
     ])
     return Promise.resolve()
   }
@@ -114,25 +114,6 @@ const relay = async (channel, sender, inboundMsg) => {
   } catch (e) {
     logger.error(e)
   }
-}
-
-// (Database, Socket, SdMessage, number) -> Promise<void>
-const notifyRateLimitedMessage = async (sdMessage, resendInterval) => {
-  const channel = await channelRepository.findDeep(supportPhoneNumber)
-  if (!channel) return Promise.resolve()
-
-  const recipients = channelRepository.getAdminMemberships(channel)
-  return Promise.all(
-    recipients.map(({ memberPhoneNumber, language }) =>
-      signal.sendMessage(
-        memberPhoneNumber,
-        sdMessageOf(
-          { phoneNumber: supportPhoneNumber },
-          messagesIn(language).notifications.rateLimitOccurred(sdMessage.username, resendInterval),
-        ),
-      ),
-    ),
-  )
 }
 
 const updateFingerprint = async updatableFingerprint => {
