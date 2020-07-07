7cd8e08ff50c110dad3b375c5049de4b16d977fe
Mari <m@marrri.com>
2020-07-07 15:20:05 -0400

    [ni] add stripe integration and BTC wallet address

--- splash/gatsby-node.js
+++ splash/gatsby-node.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
---
+++ splash/src/components/stripe.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+
+const buttonStyles = {
+  fontSize: '20px',
+  textAlign: 'center',
+  color: '#000',
+  padding: '12px 25px',
+  backgroundColor: '#bd93f9',
+  borderRadius: '5px',
+  border: 'none',
+  margin: '10px',
+}
+
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+
+const redirectToCheckout = async (event, priceID) => {
+  event.preventDefault()
+  const stripe = await stripePromise
+  const { error } = await stripe.redirectToCheckout({
+    lineItems: [{ price: priceID, quantity: 1 }],
+    mode: 'payment',
+    successUrl: `https://signalboost.info/donate`,
+    cancelUrl: `https://signalboost.info/donate`,
+  })
+
+  if (error) {
+    console.warn('Error:', error)
+  }
+}
+
+const Checkout = ({ price, priceID }) => {
+  return (
+    <button style={buttonStyles} onClick={e => redirectToCheckout(e, priceID)}>
+      {price}
+    </button>
+  )
+}
+
+export default Checkout
