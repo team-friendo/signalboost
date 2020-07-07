a0a43ce8892da20186f900d654aa6d781f3350a5
Mari <m@marrri.com>
2020-07-07 19:27:22 -0400

    [ni] use .env.dev for development and .env for prod

--- splash/gatsby-node.js
+++ splash/gatsby-node.js
@@ -3,8 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-// You can delete this file if you're not using it
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+const path = activeEnv === 'production' ? '.env' : '.env.dev'
+require('dotenv').config({ path })
