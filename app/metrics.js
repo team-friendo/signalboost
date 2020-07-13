3cbc6779cf81934ab2865c85c73457b7dd90869c
aguestuser <aguestuser@riseup.net>
2020-07-13 18:08:10 -0400

    [264] add msg roundtrip histogram, explain why gauge is suboptimal

--- app/metrics.js
+++ app/metrics.js
@@ -11,7 +11,11 @@ const _counters = {
 }
 
 const _gauges = {
-  MESSAGE_LAG: 'MESSAGE_LAG',
+  MESSAGE_ROUNDTRIP: 'MESSAGE_ROUNDTRIP',
+}
+
+const _histograms = {
+  MESSAGE_ROUNDTRIP: 'MESSAGE_ROUNDTRIP',
 }
 
 const messageDirection = {
@@ -29,6 +33,7 @@ const run = () => {
   prometheus.collectDefaultMetrics({ registry })
   const c = _counters
   const g = _gauges
+  const h = _histograms
 
   const counters = {
     [c.ERRORS]: new prometheus.Counter({
@@ -60,29 +65,73 @@ const run = () => {
   }
 
   const gauges = {
-    [g.MESSAGE_LAG]: new prometheus.Gauge({
-      name: 'message_lag',
-      help: 'Times the message lag from signald',
+    // TODO (aguestuser|2020-07-13):
+    //  - `setGauge` overwrites the last gauge value and is scraped every 5 sec
+    //  - thus, when we call `metrics.gauges[g.MESSAGES_ROUNDTRIP], we are throwing away:
+    //    - 999 / 1000 data points in a healthy state (roundtrip ~= 300ms)
+    //    - 1 / 2 data points in an unhealty state (roundtrip ~= 2 sec)
+    //  - for a better solution, we'd need to:
+    //    - modify `metrics.gauges[g.MESSAGES_ROUNDTRIP] to:
+    //      - keep a collection of tuples of all roundtrip times and their labels
+    //      - implement `collect` to (1) take the max of those times, (2) apply the labels
+    //    - modify `setGauge` to mutate this collection instead of directly setting the gauge
+    //  - this is complicated enough to be worth postponing for now, but because our gauge will
+    //    therefore be imprecise (and potentially highly misleading), we will prefer the histogram
+    //    data for now
+    [g.MESSAGE_ROUNDTRIP]: new prometheus.Gauge({
+      name: 'message_roundtrip_gauge',
+      help:
+        'Measures millis elapsed between when message is enqueued for sending with signald' +
+        'and when signald confirms successful sending',
       registers: [registry],
       labelNames: ['channelPhoneNumber'],
     }),
   }
 
-  return { registry, counters, gauges }
+  const histograms = {
+    [h.MESSAGE_ROUNDTRIP]: new prometheus.Histogram({
+      name: 'message_roundtrip',
+      help:
+        'Measures millis elapsed between when message is enqueued for sending with signald' +
+        'and when signald confirms successful sending',
+      registers: [registry],
+      labelNames: ['channelPhoneNumber'],
+      // buckets increase exponentially by power of 4 from base of 500 millis
+      buckets: [
+        500, // .5 sec
+        2000, // 2 sec
+        8000, // 8 sec
+        32000, // 32 sec
+        128000, // ~2 min
+        512000, // ~8 min
+        2048000, // ~34 min
+        8092000, // ~2 hr
+      ],
+    }),
+  }
+
+  return { registry, counters, gauges, histograms }
 }
 
-// (string, [string]) -> void
+// (string, Array<string>) -> void
 const incrementCounter = (counter, labels) => app.metrics.counters[counter].labels(...labels).inc()
 
-const setGauge = (gauge, labels, value) => app.metrics.gauges[gauge].labels(...labels).set(value)
+// (string, number, Array<string>) -> void
+const setGauge = (gauge, value, labels) => app.metrics.gauges[gauge].labels(...labels).set(value)
+
+// (string, number, Array<string) -> void
+const observeHistogram = (histogram, value, labels) =>
+  app.metrics.histograms[histogram].labels(...labels).observe(value)
 
 module.exports = {
   run,
   register,
   incrementCounter,
   setGauge,
+  observeHistogram,
   counters: _counters,
   gauges: _gauges,
+  histograms: _histograms,
   messageDirection,
   errorTypes,
 }
