01f20983eb3b4acbbb4029106f1dc59548e9e0bc
Mari <m@marrri.com>
2020-07-16 17:53:10 -0400

    [ni] rewrite splash intro section; include more helpful 'getting started' section

--- splash/src/pages/index.js
+++ splash/src/pages/index.js
@@ -7,139 +7,127 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Signalboost: Secure Textblasts and Hotlines for Activists" />
     <p>
-      Signalboost lets activists use Signal to send text blasts and receive
-      hotline tips without revealing their identity or spending money. It is{' '}
-      <em style={{ color: '#ff79c6' }}>secure, focused, and free.</em>
+      Signalboost gives grassroots organizers the power to communicate with mass
+      audiences <span className="purple">securely</span> and{' '}
+      <span className="purple">directly</span> via message broadcasts and
+      hotlines.
     </p>
-    <h3>Secure:</h3>
+    <h3>Reach thousands of people directly</h3>
+    <ul>
+      <li>
+        Organizers need quick and direct ways to disseminate information.
+        However, on social media platforms, important info is often obscured by
+        algorithms and other posts. Signalboost messages go directly to
+        subscribers' message inboxes.
+      </li>
+      <li>
+        Organizing in big group chats gets messy quickly and unnecessarily
+        exposes strangers' phone numbers to each other. Broadcasts offer a quick
+        way disseminate information directly to thousands of people.
+      </li>
+    </ul>
+    <h3>Respond to people individually and privately</h3>
+    <ul>
+      <li>
+        Subscribers to Signalboost channels can send in anonymous messages to
+        the hotline and admins can respond directly and privately to that
+        susbcriber.
+      </li>
+      <li>
+        Subscribers do not see other subscribers' messages to the hotline. Only
+        admins can see them.
+      </li>
+    </ul>
+    <h3>Stay safe from censorship and surveillance</h3>
     <ul>
       <li>
         Signalboost sends messages over <a href="https://signal.org">Signal</a>,
-        the most secure encrypted messaging service available for phones.
+        the most secure encrypted messaging service for phones.
       </li>
       <li>
-        It does not display phone numbers or names. People can send and receive
-        messages without sharing their identity and be safer if anyone's phone
-        is stolen or seized.
-      </li>
-      <li>
-        Signalboost retains the minimal user metadata necessary to route
-        messages. Its maintainers, team-friendo, will resist any attempt to
-        compel us disclose it, and are working on{' '}
-        <a href="https://0xacab.org/team-friendo/signalboost/issues/68">
-          updates
-        </a>{' '}
-        make such disclosure impossible.
+        By obscuring personal phone numbers, enforcing disappearing messages,
+        and controlling who is allowed to join their channel, Signalboost
+        empowers organizers to stay safe while speaking freely.
       </li>
     </ul>
-    <h3>Focused:</h3>
-    <ul>
-      <li>
-        Signalboost is for 1-way messaging only. Admins can send announcements
-        to large groups, or receive and reply to hotline messages from anyone.
-        That's it.
-      </li>
-      <li>
-        Signalboost does not allow subscribers to send messages to each other.
-        This cuts out cross talk endemic to large Signal or WhatsApp groups.
-      </li>
-      <li>
-        It tries to solve one problem and solve it well. This makes it easier to
-        know what it's for amidst a dizzying sea of tech tools!
-      </li>
-    </ul>
-    <h3>Free:</h3>
-    <ul>
-      <li>
-        We are making this software for people's collective liberation, not for
-        profit. We do not charge money for it and never will.
-      </li>
-    </ul>
-    <h2>Getting Started</h2>
-    <h3>Try joining a channel:</h3>
+    <h2 id="getting-started" className="anchor">
+      Getting Started
+    </h2>
+    <h3 className="getting-started-header">
+      1.{' '}
+      <a className="download-signal-link" href="https://signal.org/download/">
+        Download Signal
+      </a>
+    </h3>
+    <h3 className="getting-started-header anchor">
+      2. Subscribe to the Signalboost Announcements and Helpline channel
+    </h3>
     <p>
-      Send HELLO to <span className="purple">+1-938-222-9889</span> to join our
-      Signalboost playground channel.
+      Send <span className="command">HELLO</span> to{' '}
+      <span className="purple">+1-947-800-5717</span> (that's 947-BOOST-IT!){' '}
     </p>
-    <p>Send HELP to see the commands you can use and GOODBYE to leave.</p>
-    <h3>Get your own channel:</h3>
+    <p>
+      Send <span className="command">INFO</span> to see details about the
+      channel.
+    </p>{' '}
+    <p>
+      Send <span className="command">HELP</span> to see the other commands you
+      can use.
+    </p>
+    <h3 className="getting-started-header">3. Request your own channel:</h3>
     <p>
       Send a Signal message to <span className="purple">+1-947-800-5717</span>{' '}
-      that includes a name for the channel and the phone numbers of at least 2
-      admins. (You can remember that as:{' '}
-      <span className="purple">947 BOOST IT!</span>)
+      that includes channel name and the phone numbers of at least 2 admins. For
+      example:
     </p>
+    <blockquote className="channel-request">
+      <p>Test Channel</p>
+      <p>+1-123-555-5555, +1-123-555-5555</p>
+    </blockquote>
     <p>
       You will receive a welcome message as soon as your channel is created.
     </p>
     <p>
-      You can also write team-friendo with support questions at any time on this
-      channel , and we will do our best to respond promptly.
+      You can write our helpline with questions or a request for a more in-depth
+      training, and we'll get back to you asap!
     </p>
-    <h3>Once you have a channel:</h3>
-    <ul>
-      <li>
-        There will be a Signal phone number associated with your channel.
-        Anytime you send an announcement to your channel's phone number, anyone
-        who is subscribed will get the announcement.
-      </li>
-      <li>
-        Your friends can subscribe to announcements by sending a Signal message
-        to your channel phone number that says "HELLO." ("HOLA", "SALUT", or
-        "HALLO" also work!) They can unsubscribe by sending "GOODBYE." ("ADIÓS",
-        "AREVOIR", or "TSCHÜSS" also work!)
-      </li>
-      <li>
-        You can convert the channel into a hotline by sending a message that
-        says "HOTLINE ON" to channel number.
-      </li>
-      <li>
-        Signalboost speaks English, Spanish, French, and German. Each user can
-        specify their own personal language preference by sending a message with
-        the name of the language they want to use to the channel. (E.g., to
-        change the channel language to Spanish, you would send "ESPAÑOL.")
-      </li>
-      <li>
-        All users can send a message to the channel that says "HELP" to list all
-        command options or "INFO" to learn basic information about the channel.
-      </li>
-    </ul>
+    <h3 className="getting-started-header">
+      4. Get subscribers and send announcements
+    </h3>
+    <p>
+      Now, any anyone who sends "HELLO" to your channel number will get
+      announcements you send out!
+    </p>
+    <p>
+      Check out our <Link to="/how-to">How-To Guide</Link> to get started with
+      your channel, learn about other features, and supported languages.
+    </p>
     <h2>Got questions?</h2>
-    <ul>
-      <li>
-        Check out the <Link to="/faq">FAQ</Link> or{' '}
-        <Link to="/how-to">How-To Manual</Link>
-      </li>
-      <li>
-        Try out commands by Signal-messaging the playground channel at:
-        <span className="purple">+1-938-222-9889</span>
-      </li>
-      <li>
-        Talk to us directly by Signal-messaging our support channel at:{' '}
-        <span className="purple">+1-947-800-5717</span> (That's <span className="purple">"947 BOOST IT!"</span>)
-      </li>
-      <li>
-        Send us an email at{' '}
-        <a href="mailto:team-friendo@protonmail.com">
-          team-friendo@protonmail.com
-        </a>{' '}
-        or <a href="mailto:team-friendo@riseup.net">team-friendo@riseup.net</a>.
-        (Find our{' '}
-        <a href="http://keys.gnupg.net/pks/lookup?search=0xE726A156229F56F1&fingerprint=on&op=index">
-          PGP key here
-        </a>
-        .)
-      </li>
-      <li>
-        Visit our&nbsp;
-        <a href="https://0xacab.org/team-friendo/signalboost">gitlab page</a>
-        &nbsp;to read our source code and{' '}
-        <a href="https://0xacab.org/team-friendo/signalboost/-/wikis/Team-Friendo-Values-and-CoC">
-          code of conduct
-        </a>
-        , request a new feature, report a bug, or become a contributor!
-      </li>
-    </ul>
+    <p>
+      Check out the <Link to="/faq">FAQ</Link> or{' '}
+      <Link to="/how-to">How-To Guide</Link>
+    </p>
+    <p>
+      Send us an email at{' '}
+      <a href="mailto:team-friendo@protonmail.com">
+        team-friendo@protonmail.com
+      </a>{' '}
+      or <a href="mailto:team-friendo@riseup.net">team-friendo@riseup.net</a>.
+      (Find our{' '}
+      <a href="http://keys.gnupg.net/pks/lookup?search=0xE726A156229F56F1&fingerprint=on&op=index">
+        PGP key here
+      </a>
+      .)
+    </p>
+    <p>
+      Signalboost is completely open source & committed to transparency. You can
+      read our{' '}
+      <a href="https://0xacab.org/team-friendo/signalboost">source code</a> and{' '}
+      <a href="https://0xacab.org/team-friendo/signalboost/-/wikis/Team-Friendo-Values-and-CoC">
+        code of conduct
+      </a>{' '}
+      on Gitlab.
+    </p>
   </Layout>
 )
 
