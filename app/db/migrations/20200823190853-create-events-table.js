96ecfe45c65c21013a430ba96fe08eccb432d033
aguestuser <aguestuser@riseup.net>
2020-08-26 18:06:36 -0400

    [328] add backfill for event log
    * query for channel and (unique) member creations
    * create event records for them

--- app/db/migrations/20200823190853-create-events-table.js
+++ app/db/migrations/20200823190853-create-events-table.js
@@ -27,7 +27,64 @@ module.exports = {
         type: Sequelize.DATE,
       },
     })
-    
+
+    const eventCount = await queryInterface.sequelize.query(
+      'select count(*) from events',
+      { type: Sequelize.QueryTypes.SELECT }
+    ).then(([{count}]) => parseInt(count))
+
+    console.log(`found ${eventCount} pre-existing events...`)
+
+    if (eventCount === 0) {
+      console.log('starting backfill...')
+
+      const genUuid = require('uuid/v4')
+
+      const hash = str =>
+        require('crypto')
+          .createHash('sha256')
+          .update(str + process.env.SIGNALBOOST_HASH_SALT)
+          .digest('hex')
+
+      const channels = await queryInterface.sequelize.query(
+        'select "phoneNumber", "createdAt" from channels',
+        { type: Sequelize.QueryTypes.SELECT }
+      )
+
+      if (channels.length > 0) {
+        console.log(`logging creation of ${channels.length} channels...`)
+        await queryInterface.bulkInsert('events', channels.map(({ phoneNumber, createdAt }) => ({
+          id: genUuid(),
+          type: 'CHANNEL_CREATED',
+          phoneNumberHash: hash(phoneNumber),
+          createdAt,
+          updatedAt: createdAt,
+        })))
+      }
+
+      const members = await queryInterface.sequelize.query(`
+      select "memberPhoneNumber" as "phoneNumber", min("createdAt") "createdAt"
+        from memberships
+        group by "memberPhoneNumber"
+        order by "createdAt";
+      `,
+        { type: Sequelize.QueryTypes.SELECT }
+      )
+
+      if (members.length > 0) {
+        console.log(`logging creation of ${members.length} members...`)
+        await queryInterface.bulkInsert('events', members.map(({ phoneNumber, createdAt }) => ({
+          id: genUuid(),
+          type: 'MEMBER_CREATED',
+          phoneNumberHash: hash(phoneNumber),
+          createdAt,
+          updatedAt: createdAt,
+        })))
+      }
+
+      console.log('...backfill succeeded!')
+    }
+
     return queryInterface.addIndex('events', {
       fields: ['phoneNumberHash']
     })
