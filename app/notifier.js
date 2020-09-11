7a0e5b40e730d6d11be73202b4156c13eb233f8e
aguestuser <aguestuser@riseup.net>
2020-09-11 12:59:03 -0400

    [hotfix] notify maintainers on the diagnostics channel
    * we used to notify ourselves on BOOST IT, now we tend to get
      notifications on `DIAGNOSTICS`. let's be consistent about that!
    * while at it: send these notifications in sequence. if a big channel
      is destroyed or recycled, we don't want signald to shit the bed
    * aaaand... let's make `_notifyMany` handle either a concrete message
      or a notification key so we can use it as a helper for all of our
      notify functions

--- app/notifier.js
+++ app/notifier.js
@@ -1,11 +1,11 @@
 const channelRepository = require('./db/repositories/channel')
 const signal = require('./signal')
+const { sequence } = require('./util')
 const { getAdminMemberships } = require('./db/repositories/channel')
-const { getAdminPhoneNumbers } = require('./db/repositories/channel')
 const { messagesIn } = require('./dispatcher/strings/messages')
 const { sdMessageOf } = require('./signal/constants')
 const {
-  signal: { supportPhoneNumber },
+  signal: { diagnosticsPhoneNumber },
 } = require('./config')
 
 const notificationKeys = {
@@ -17,32 +17,36 @@ const notificationKeys = {
 // (Channel, String, String) -> Promise<Array<string>>
 const notifyMembersExcept = async (channel, sender, notificationKey) => {
   const recipients = channelRepository.getMembersExcept(channel, [sender])
-  return _notifyMany(channel, notificationKey, recipients)
+  return _notifyMany({ channel, notificationKey, recipients })
 }
 
 // (string) -> Promise<Array<string>>
 const notifyMaintainers = async message => {
-  if (!supportPhoneNumber) return Promise.resolve([])
-  const supportChannel = await channelRepository.findDeep(supportPhoneNumber)
-  const maintainerPhoneNumbers = getAdminPhoneNumbers(supportChannel)
-  await signal.broadcastMessage(maintainerPhoneNumbers, sdMessageOf(supportChannel, message))
+  if (!diagnosticsPhoneNumber) return Promise.resolve([])
+  const channel = await channelRepository.findDeep(diagnosticsPhoneNumber)
+  const recipients = getAdminMemberships(channel)
+  return _notifyMany({ channel, recipients, message })
 }
 
 // (string, string) -> Promise<Array<string>>
 const notifyAdmins = async (channel, notificationKey) =>
-  _notifyMany(channel, notificationKey, getAdminMemberships(channel))
+  _notifyMany({ channel, notificationKey, recipients: getAdminMemberships(channel) })
 
 // (Channel, string) -> Promise<Array<string>>
 const notifyMembers = async (channel, notificationKey) =>
-  _notifyMany(channel, notificationKey, channel.memberships)
+  _notifyMany({ channel, notificationKey, recipients: channel.memberships })
 
 // (Channel, string, Array<Member>) => Promise<Array<string>>
-const _notifyMany = (channel, notificationKey, recipients) =>
-  Promise.all(
-    recipients.map(recipient =>
+const _notifyMany = ({ channel, recipients, notificationKey, message }) =>
+  // TODO(aguestuser|2020-09-11):
+  //  we sequence these to get around signald concurrency bugs, eventually use Promise.all here
+  sequence(
+    recipients.map(recipient => () =>
       signal.sendMessage(
         recipient.memberPhoneNumber,
-        sdMessageOf(channel, messagesIn(recipient.language).notifications[notificationKey]),
+        message
+          ? sdMessageOf(channel, message)
+          : sdMessageOf(channel, messagesIn(recipient.language).notifications[notificationKey]),
       ),
     ),
   )
