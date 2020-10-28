be3ed4d20ba65d6125af3db7b48b3fc39f3f9dcc
fdbk <fdbk@riseup.net>
2020-10-28 13:30:47 -0400

    [fdbk] Add simulator directory with socket, signal, callback modules.

---
+++ simulator/callbacks.js
@@ -0,0 +1,107 @@
+const { messageTypes } = require('../app/constants')
+const util = require('../app/util')
+const { statuses } = util
+const { get } = require('lodash')
+const {
+  signal: { signaldVerifyTimeout },
+} = require('../app/config')
+
+/**
+ * type Callback = ({
+ *   message: IncomingSignaldMessage | SendResponse,
+ *   ?resolve: (any) => void,
+ *   ?reject: (any) => void,
+ *   ?state: CbState,
+ * }) => void | Promise<void>
+ *
+ * type CbState = {
+ *   ?channelPhoneNumber: string,
+ *   ?messageBody: string,
+ *   ?attachments: Array<SignaldOutboundAttachment>,
+ *   ?whenSent: number, // millis since epoch
+ * }
+ * type CallbackRegistry {
+ *   [id: string]: {
+ *     callback: Callback,
+ *     resolve: ((any) => void) | null,
+ *     reject: ((any) => void) | null,
+ *     state: object | null,
+ *   }
+ * }
+ ***/
+
+const messages = {
+  timeout: messageType => `Signald response timed out for request of type: ${messageType}`,
+  verification: {
+    error: (phoneNumber, reason) =>
+      `Signal registration failed for ${phoneNumber}. Reason: ${reason}`,
+  },
+}
+
+// CallbackRegistry
+const registry = {}
+const clear = () => Object.keys(registry).forEach(k => delete registry[k])
+
+// ({ messageType: SingaldMessageType, id: string, resolve: function, reject: function, state: object }) -> Promise<void>
+const register = async ({ messageType, id, resolve, reject, state }) => {
+  registry[`${messageType}-${id}`] = { callback: _callbackFor(messageType), resolve, reject, state }
+  const timeout = _timeoutFor(messageType)
+  return util.wait(timeout).then(() => {
+    // delete expired callbacks and reject their promise if not yet handled
+    delete registry[`${messageType}-${id}`]
+    reject && reject({ status: statuses.ERROR, message: messages.timeout(messageType) })
+  })
+}
+
+// SignaldMessageType -> Callback
+const _callbackFor = messageType =>
+  ({
+    [messageTypes.VERIFY]: _handleVerifyResponse,
+  }[messageType])
+
+// SignaldMessageType => number
+const _timeoutFor = messageType =>
+  ({
+    [messageTypes.VERIFY]: signaldVerifyTimeout,
+  }[messageType])
+
+// (IncomingSignaldMessage | SendResponse) -> CallbackRoute
+const handle = message => {
+  // called from dispatcher.relay
+  const { callback, resolve, reject, state } = {
+    [messageTypes.VERIFICATION_SUCCESS]:
+      registry[`${messageTypes.VERIFY}-${get(message, 'data.username')}`],
+    [messageTypes.VERIFICATION_ERROR]:
+      registry[`${messageTypes.VERIFY}-${get(message, 'data.username')}`],
+  }[message.type] || { callback: util.noop }
+  callback({ message, resolve, reject, state })
+}
+
+// CALLBACKS
+
+// (IncomingSignaldMessage, function, function) -> void
+const _handleVerifyResponse = ({ message, resolve, reject }) => {
+  if (message.type === messageTypes.VERIFICATION_ERROR)
+    reject(
+      new Error(
+        messages.verification.error(
+          get(message, 'data.username', 'N/A'),
+          get(message, 'data.message', 'Captcha required: 402'),
+        ),
+      ),
+    )
+  if (message.type === messageTypes.VERIFICATION_SUCCESS)
+    resolve({
+      status: statuses.SUCCESS,
+      message: get(message, 'data.username', 'N/A'),
+    })
+}
+
+module.exports = {
+  messages,
+  registry,
+  clear,
+  handle,
+  register,
+  _handleVerifyResponse,
+}
---
+++ simulator/socket/index.js
@@ -0,0 +1,71 @@
+const { createPool } = require('generic-pool')
+const net = require('net')
+const fs = require('fs-extra')
+const { wait, loggerOf } = require('../../app/util')
+const {
+  socket: { connectionInterval, maxConnectionAttempts, poolSize },
+} = require('../../app/config')
+
+// CONSTANTS
+
+const logger = loggerOf('socket')
+const SIGNALD_SOCKET_PATH = '/var/run/signald/signald.sock'
+const messages = {
+  error: {
+    socketTimeout: 'Maximum signald connection attempts exceeded.',
+    socketConnectError: reason => `Failed to connect to signald socket; Reason: ${reason}`,
+  },
+}
+
+// STARTUP
+
+// () => Promise<Pool>
+const run = async () => {
+  logger.log('Initializing socket pool...')
+  const pool = await socketPoolOf({ create: getSocketConnection, destroy: destroySocketConnection })
+  logger.log(`...initialized pool of ${pool.size} sockets.`)
+  return pool
+}
+
+/* ({ create: () => Socket, destroy: () => void}) -> Promise<void> */
+const socketPoolOf = async ({ create, destroy }) => {
+  const pool = await createPool({ create, destroy }, { min: poolSize, max: poolSize })
+  pool.stop = () => pool.drain().then(() => pool.clear())
+  return pool
+}
+
+// number -> Promise<Socket>
+const getSocketConnection = async (attempts = 0) => {
+  if (!(await fs.pathExists(SIGNALD_SOCKET_PATH))) {
+    if (attempts > maxConnectionAttempts) {
+      return Promise.reject(new Error(messages.error.socketTimeout))
+    } else {
+      return wait(connectionInterval).then(() => getSocketConnection(attempts + 1))
+    }
+  } else {
+    return connect()
+  }
+}
+
+// () -> Promise<Socket>
+const connect = () => {
+  try {
+    const sock = net.createConnection(SIGNALD_SOCKET_PATH)
+    sock.setEncoding('utf8')
+    sock.setMaxListeners(0) // removes ceiling on number of listeners (useful for `await` handlers below)
+    sock.on('data', msg => dispatcher.dispatch(msg).catch(logger.error))
+    return new Promise(resolve => sock.on('connect', () => resolve(sock)))
+  } catch (e) {
+    return Promise.reject(new Error(messages.error.socketConnectError(e.message)))
+  }
+}
+
+// Socket -> void
+const destroySocketConnection = sock => sock.destroy()
+
+module.exports = {
+  run,
+  getSocketConnection,
+  destroySocketConnection,
+  socketPoolOf,
+}
