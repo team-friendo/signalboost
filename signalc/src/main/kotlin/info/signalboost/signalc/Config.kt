6466d4705f1ea30aeab6fc21d56fcb13f011d01f
aguestuser <aguestuser@riseup.net>
2021-01-14 14:40:20 -0500

    [394] failed attempt to parse yaml using an intermediate hashmap
    * flounders on compile errors due to failure to provide a serializer
      for `HashMap<String,Any>` womp womp.
    * leaving this here as a breadcrumb if we ever want to come back and
      try again!

--- signalc/src/main/kotlin/info/signalboost/signalc/Config.kt
+++ signalc/src/main/kotlin/info/signalboost/signalc/Config.kt
@@ -2,16 +2,23 @@ package info.signalboost.signalc
 
 import com.charleskorn.kaml.Yaml
 import kotlinx.serialization.*
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.Contextual
+import org.h2.util.JdbcUtils.serializer
 import java.io.File
 import kotlin.system.exitProcess
 
 object Config {
+    // CONSTANTS
+
     const val USER_PHONE_NUMBER = "+17347962920"
-    // NOTE: for signalc to run outside docker, you must export project root as an env var
     val projectRoot: String = System.getenv("SIGNALC_PROJECT_ROOT") ?: run {
         println("\$SIGNALC_PROJECT_ROOT must be defined. Aborting.")
         exitProcess(1)
     }
+
+    // FACTORIES
 
     enum class Env(val value: String) {
         Default("default"),
@@ -20,6 +27,10 @@ object Config {
         Prod("prod"),
     }
 
+    val prod: App by lazy { fromEnvs(listOf(Env.Default)) }
+    val dev: App by lazy { fromEnv(Env.Dev) }
+    val test : App by lazy { fromEnv(Env.Test) }
+
     fun fromEnv(env: Env): App = fromEnvs(listOf(Env.Default,env))
 
     fun fromEnvs(envs: List<Env>): App =
@@ -27,19 +38,44 @@ object Config {
             .map { File("${projectRoot}/config.${it.value}.yml").readText(Charsets.UTF_8) }
             .let { fromYamls(it) }
 
+    // Merge configs from an arbitrarily long sequence of yaml files such that:
+    // (1) the first yaml in the list will be considered the "base" (and should provide defautls for all fields)
+    // (2) subsequent yamls may override fields from the base yaml
+    // (3) subsequent yamls may only specify a subset of all fiels (so long as all fields are defined in the base)
+    // -
+    // inspired by: https://github.com/bodylabs/misty-config
     fun fromYamls(yamls: List<String>): App =
-        yamls.fold(AppYaml.empty) { acc, yaml ->
-            acc.merge(
-                Yaml.default.decodeFromString(AppYaml.serializer(), yaml)
+        yamls
+            // this naive approach doesn't compile (yields: Serializer for class 'Any' is not found.)
+            // .map { Yaml.default.decodeFromString(serializer(), it) as HashMap<String,Any> }
+            // ---
+            // but this less naive approach ALSO doesn't compile (see below)
+            .map { myYamlModule.decodeFromString(serializer<HashMap<String,@Contextual Any>>(), it) as HashMap<String,Any> }
+            .reduce { acc, item -> acc.merge(item) }
+            .toConfig()
+
+    // this attempated solution (as per https://github.com/Kotlin/kotlinx.serialization/issues/1019)
+    // yields:
+    // ```
+    // Unresolved reference. None of the following candidates is applicable because of receiver type mismatch:
+    // public fun String.compareTo(other: String, ignoreCase: Boolean = ...): Int defined in kotlin.text
+    // ```
+    val myYamlModule = Yaml(
+        serializersModule = SerializersModule {
+            contextual(String::class, String.serializer())
+            contextual(String::class, String.serializer())
+            contextual(Int::class, Int.serializer())
+            contextual(Boolean::class, Boolean.serializer())
+            // compile error triggered on this line --v
+            contextual(
+                HashMap::class,
+                serializer<HashMap<String,@Contextual Any>()
             )
-        }.unwrap()
+        }
+    )
 
-    val prod: App by lazy { fromEnvs(listOf(Env.Default)) }
-    val dev: App by lazy { fromEnv(Env.Dev) }
-    val test : App by lazy { fromEnv(Env.Test) }
 
-    // TODO(aguestuser|2021-01-13):
-    //   HOLY CRAP this is a lot of boilerplate! look into how we could use reflection to DRY it up?
+    // CONFIG SCHEMA
 
     data class App(
         val db: Database,
@@ -47,56 +83,11 @@ object Config {
         val store: Store,
     )
 
-    @Serializable
-    data class AppYaml(
-        val db: DatabaseYaml? = null,
-        val signal: SignalYaml? = null,
-        val store: StoreYaml? = null,
-    ) {
-        companion object {
-            val empty = AppYaml(
-                null,
-                null,
-                null,
-            )
-
-        }
-        fun unwrap() = App(
-            db!!.unwrap(),
-            signal!!.unwrap(),
-            store!!.unwrap(),
-        )
-        fun merge(other: AppYaml) = AppYaml(
-            db = db?.merge(other.db) ?: other.db,
-            signal = signal?.merge(other.signal) ?: other.signal,
-            store = store?.merge(other.store) ?: other.store,
-        )
-    }
-
     data class Database(
         val driver: String,
         val url: String,
         val user: String,
     )
-
-    @Serializable
-    data class DatabaseYaml(
-        val driver: String? = null,
-        val url: String? = null,
-        val user: String? = null,
-    ) {
-        fun unwrap() = Database(
-            driver = driver!!,
-            url = url!!,
-            user = user!!,
-        )
-
-        fun merge(other: DatabaseYaml?) = DatabaseYaml(
-            driver = other?.driver ?: driver,
-            url = other?.url ?: url,
-            user = other?.user ?: user,
-        )
-    }
 
     data class Signal(
         val addSecurityProvider: Boolean,
@@ -112,72 +103,70 @@ object Config {
         val storageUrl: String,
     )
 
-    @Serializable
-    data class SignalYaml(
-        val addSecurityProvider: Boolean? = null,
-        val agent: String? = null,
-        val trustStorePath: String? = null,
-        val trustStorePassword: String? = null,
-        val zkGroupServerPublicParams: String? = null,
-        val serviceUrl: String? = null,
-        val cdnUrl: String? = null,
-        val cdn2Url: String? = null,
-        val contactDiscoveryUrl: String? = null,
-        val keyBackupServiceUrl: String? = null,
-        val storageUrl: String? = null,
-    ) {
-        fun unwrap() = Signal(
-            addSecurityProvider = addSecurityProvider!!,
-            agent = agent!!,
-            trustStorePath = trustStorePath!!,
-            trustStorePassword = trustStorePassword!!,
-            zkGroupServerPublicParams = zkGroupServerPublicParams!!,
-            serviceUrl = serviceUrl!!,
-            cdnUrl = cdnUrl!!,
-            cdn2Url = cdn2Url!!,
-            contactDiscoveryUrl = contactDiscoveryUrl!!,
-            keyBackupServiceUrl = keyBackupServiceUrl!!,
-            storageUrl = storageUrl!!,
-        )
-        fun merge(other: SignalYaml?) = SignalYaml(
-            addSecurityProvider = other?.addSecurityProvider ?: addSecurityProvider,
-            agent = other?.agent ?: agent,
-            trustStorePath = other?.trustStorePath ?: trustStorePath,
-            trustStorePassword = other?.trustStorePassword ?: trustStorePassword,
-            zkGroupServerPublicParams = other?.zkGroupServerPublicParams ?: zkGroupServerPublicParams,
-            serviceUrl = other?.serviceUrl ?: serviceUrl,
-            cdnUrl = other?.cdnUrl ?: cdnUrl,
-            cdn2Url = other?.cdn2Url ?: cdn2Url,
-            contactDiscoveryUrl = other?.contactDiscoveryUrl ?: contactDiscoveryUrl,
-            keyBackupServiceUrl = other?.keyBackupServiceUrl ?: keyBackupServiceUrl,
-            storageUrl = other?.storageUrl ?: storageUrl,
-        )
-    }
-
     enum class StoreType(value: String) {
         SQL("SQL"),
         MOCK("MOCK"),
     }
-
 
     data class Store(
         val account: StoreType,
         val signalProtocol: StoreType,
     )
 
-    @Serializable
-    data class StoreYaml(
-        val account: StoreType? = null,
-        val signalProtocol: StoreType? = null,
-    ) {
-        fun unwrap() = Store(
-            account = account!!,
-            signalProtocol = signalProtocol!!,
-        )
-        fun merge(other: StoreYaml?) = StoreYaml(
-            account = other?.account ?: account,
-            signalProtocol = other?.signalProtocol ?: signalProtocol,
+    // GLUE CODE (for merging hash maps and coercing them to the the config schema)
+
+    @Suppress("UNCHECKED_CAST")
+    fun HashMap<String,Any>.toConfig(): App {
+
+        val db = this["db"] as HashMap<String, Any>
+        val signal = this["signal"] as HashMap<String, Any>
+        val store = this["store"] as HashMap<String, Any>
+
+        fun HashMap<String,Any>.unwrap(value: Any): Any = this[value]!!.let {
+            if(it is String){
+                if(it[0] == '$') return System.getenv(it.substring(1))!!
+                if(it == "SQL") return StoreType.SQL
+                if(it == "MOCK") return StoreType.MOCK
+            }
+            return it
+        }
+
+        // TODO: consider some sort of meta-programming to clean this up...
+        return App(
+            db = Database(
+                driver = db.unwrap("driver") as String,
+                url = db.unwrap("url") as String,
+                user = db.unwrap("user") as String,
+            ),
+            signal = Signal(
+                addSecurityProvider = db.unwrap("addSecurityProvider") as Boolean,
+                agent = signal.unwrap("agent") as String,
+                trustStorePath = signal.unwrap("trustStorePath") as String,
+                trustStorePassword = signal.unwrap("trustStorePassword") as String,
+                zkGroupServerPublicParams = signal.unwrap("zkGroupServerPublicParams") as String,
+                serviceUrl = signal.unwrap("serviceUrl") as String,
+                cdnUrl = signal.unwrap("cdnUrl") as String,
+                cdn2Url = signal.unwrap("cdn2Url") as String,
+                contactDiscoveryUrl = signal.unwrap("contactDiscoveryUrl") as String,
+                keyBackupServiceUrl = signal.unwrap("keyBackupServiceUrl") as String,
+                storageUrl = signal.unwrap("storageUrl") as String,
+
+                ),
+            store = Store(
+                account = store.unwrap("account") as StoreType,
+                signalProtocol = store.unwrap("signalProtocol") as StoreType,
+            ),
         )
     }
+
+    @Suppress("UNCHECKED_CAST")
+    fun HashMap<String,Any>.merge(other: HashMap<String,Any>): HashMap<String,Any> =
+        this.mapValues {
+            when(val otherValue = other[it.key]) {
+                is HashMap<*,*> -> (it.value as HashMap<String,Any>).merge(otherValue as HashMap<String, Any>)
+                null -> it.value
+                else -> otherValue
+            }
+        } as HashMap<String, Any>
 
 }
