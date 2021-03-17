90fb50287b46951c979ee3374b9e3a75f15e6f4b
aguestuser <aguestuser@riseup.net>
2021-03-17 16:18:06 -0400

    [201] refactor simulator seed code (to make easier to extract)
    * pull seed functionality out of `app` module, into seed function that
      we can call as standalone script
    * extract paramaters that we could easily pass to a pure function in `app`

--- simulator/index.js
+++ simulator/index.js
@@ -1,40 +1,8 @@
-const { util } = require('chai')
-const { range, isEmpty } = require('lodash')
-const { sequence, wait } = require('../app/util')
+const { isEmpty } = require('lodash')
 
 const app = {
   socketPool: null,
 }
-
-app.seed = async () => {
-  await app.run()
-  // const signalService = require('./signal')
-
-  // const botPhoneNumbers = range(firstBotNumber, firstBotNumber + botCount).map(pn => "+" + pn)
-  // return sequence(botPhoneNumbers.map(botPN => () => signalService.registerAndVerify(botPN))).catch(logger.fatalError)
-  // return Promise.all(botPhoneNumbers.map(signalService.registerAndVerify)).catch(logger.fatalError)
-
-  const firstBotNumber = 10000000000
-  return seedOnce(firstBotNumber)
-}
-
-const seedOnce = async (firstBotNumber) => {
-  const signalService = require('./signal')
-  const { logger } = require('../app/util')
-
-  if (firstBotNumber >= 10000000000) {
-    return Promise.resolve()
-  } else {
-    const botCount = 50
-    const botPhoneNumbers = range(firstBotNumber, firstBotNumber + botCount).map(pn => "+" + pn)
-    console.log(`Seeding from ${firstBotNumber} to ${firstBotNumber + (botCount - 1)}...`)
-    await Promise.all(botPhoneNumbers.map(signalService.registerAndVerify)).catch(logger.fatalError)
-    console.log(`...seeded from ${firstBotNumber} to ${firstBotNumber + (botCount - 1)}!`)
-    await wait(5000)
-    return seedOnce(firstBotNumber + botCount)
-  }
-}
-
 
 app.run = async (botPhoneNumbers = []) => {
   const { logger } = require('../app/util')
@@ -42,10 +10,10 @@ app.run = async (botPhoneNumbers = []) => {
   const signalService = require('./signal')
 
   app.socketPool = await socketService.run().catch(logger.fatalError)
-  
+
   if (isEmpty(botPhoneNumbers)) return
 
-  logger.log(`--- Subscribing ${botCount} bots...`)
+  logger.log(`--- Subscribing ${botPhoneNumbers.length} bots...`)
   await signalService.run(botPhoneNumbers).catch(logger.fatalError)
 }
 
