543836a1c02e347358aede375f7d5ca5594cdded
aguestuser <aguestuser@riseup.net>
2021-03-19 19:02:45 -0400

    [201] refactor testLag

--- simulator/testLag.js
+++ simulator/testLag.js
@@ -1,40 +1,32 @@
 const app = require('./app')
 const { mean, max, min, take, map } = require('lodash')
-const { signalcPhoneNumbers, signaldPhoneNumbers, botPhoneNumbers } = require('./constants')
+const {
+  signalcPhoneNumbers,
+  signaldPhoneNumbers,
+  botPhoneNumbers,
+  numBots,
+} = require('./constants')
 const { nowInMillis, nowTimestamp, loggerOf, wait } = require('../app/util')
 const logger = loggerOf('testLag')
 
-/**
- * TODO:
- * - docker-compose this ish
- * - many trials?
- * - rename db
- * - containerize signalc for loadtest
- ***/
-
 ;(async () => {
   logger.log('------ STARTING LOAD TEST -------')
-
   await app.run([])
+
   const client = process.env.TEST_SUBJECT === 'sender_signalc' ? 'SIGNALC' : 'SIGNALD'
   const senderNumber = client === 'SIGNALC' ? signalcPhoneNumbers[0] : signaldPhoneNumbers[0]
-
-  // for (const numRecipients of channelSizes) {
-  for (const numRecipients of [1000]) {
-    logger.log(`Running trial for ${numRecipients} on ${client} from ${senderNumber}...`)
-    const report = await testSendingN(senderNumber, numRecipients, client)
-    logger.log(`...Completed trial for ${numRecipients} on ${client}...`)
-    print(client, report)
-  }
+  // TODO: test different numbers here
+  await testSendingN(senderNumber, numBots, client)
 
   logger.log('------- LOAD TEST COMPLETE! -------')
-
-  await wait(10000)
+  await wait(1000 * 2)
   process.exit(0)
 })()
 
 const testSendingN = async (senderNumber, numRecipients, client) => {
   const startTime = nowInMillis()
+  logger.log(`Running trial for ${numRecipients} on ${client} from ${senderNumber}...`)
+
   const timesPerMessage = await Promise.all(
     map(take(botPhoneNumbers, numRecipients), async (recipientNumber, idx) => {
       logger.log(`Sending message to ${recipientNumber}...`)
@@ -51,18 +43,23 @@ const testSendingN = async (senderNumber, numRecipients, client) => {
     }),
   )
   const endTime = nowInMillis()
-  const nonNullTimes = timesPerMessage.filter(Boolean)
+  logger.log(`...Completed trial for ${numRecipients} on ${client}...`)
 
+  print(reportOf(client, numRecipients, timesPerMessage, endTime - startTime))
+}
+
+const reportOf = (client, numRecipients, elapsedPerMessage, totalElapsed) => {
+  const nonNullTimes = elapsedPerMessage.filter(Boolean)
   return {
     client,
     numRecipients,
-    timestamp: nowTimestamp(),
     socketPoolSize: process.env.SOCKET_POOL_SIZE,
-    totalElapsed: endTime - startTime,
+    timestamp: nowTimestamp(),
+    totalElapsed,
     minElapsed: min(nonNullTimes),
     maxElapsed: max(nonNullTimes),
     meanElapsed: mean(nonNullTimes),
-    elapsedPerMessage: timesPerMessage,
+    elapsedPerMessage,
   }
 }
 
@@ -74,9 +71,9 @@ const sdMessageOf = ({ sender, recipient, message, attachments }) => ({
   attachments: attachments || [],
 })
 
-const print = (client, report) => {
+const print = report => {
   console.log('\n\n')
-  console.log(`------------------ ${client} REPORT -------------------`)
+  console.log(`------------------ LAG REPORT -------------------`)
   console.log(JSON.stringify(report, null, '  '))
   console.log('----------------------------------------------------')
 }
