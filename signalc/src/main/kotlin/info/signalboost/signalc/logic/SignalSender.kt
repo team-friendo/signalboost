7b128a6e4e0231dfd8ae8cba53b6a16840185181
aguestuser <aguestuser@riseup.net>
2021-03-26 11:25:15 -0400

    [201] use 2x + 1 parallel message queues in SignalSender

--- signalc/src/main/kotlin/info/signalboost/signalc/logic/SignalSender.kt
+++ signalc/src/main/kotlin/info/signalboost/signalc/logic/SignalSender.kt
@@ -6,8 +6,9 @@ import info.signalboost.signalc.util.CacheUtil.getMemoized
 import info.signalboost.signalc.util.TimeUtil
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.actor
 import org.whispersystems.libsignal.util.guava.Optional
 import org.whispersystems.signalservice.api.SignalServiceMessageSender
 import org.whispersystems.signalservice.api.messages.SendMessageResult
@@ -21,11 +22,10 @@ import kotlin.time.ExperimentalTime
 @ExperimentalTime
 @ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
-//@InternalCoroutinesApi
 class SignalSender(private val app: Application) {
     companion object {
         const val DEFAULT_EXPIRY_TIME = 60 * 60 * 24 // 1 day
-        val MESSAGE_QUEUE_SIZE = Application.availableProcessors
+        val paralellismFactor = (Application.availableProcessors * 2) + 1
         fun String.asAddress() = SignalServiceAddress(null, this)
         fun UUID.asAddress() = SignalServiceAddress(this, null)
     }
@@ -58,41 +58,72 @@ class SignalSender(private val app: Application) {
         val result: CompletableDeferred<SendMessageResult>,
     )
 
-    private val messageQueues = ConcurrentHashMap<String,SendChannel<QueuedMessage>>()
-    private suspend fun messageQueueOf(account: VerifiedAccount): SendChannel<QueuedMessage> =
+
+    private val messageQueues = ConcurrentHashMap<String,List<SendChannel<QueuedMessage>>>()
+    private fun messageQueueOf(account: VerifiedAccount): SendChannel<QueuedMessage> =
         getMemoized(messageQueues, account.username) {
-//            **** Actor way *******
-//            app.coroutineScope.actor(IO, MESSAGE_QUEUE_SIZE) {
-//                for (qm in channel) {
-//                    qm.result.complete(
-//                        messageSenderOf(qm.sender).sendMessage(
-//                            qm.recipient,
-//                            Optional.absent(),
-//                            qm.dataMessage
-//                        )
-//                    )
-//                }
-//            }
-//          **** Channel way *****
-            Channel<QueuedMessage>(MESSAGE_QUEUE_SIZE).also {
-                app.coroutineScope.launch(IO) {
-                    while(!it.isClosedForReceive) {
-                        val qm = it.receive()
-                        qm.result.complete(
-                            messageSenderOf(qm.sender).sendMessage(
-                                qm.recipient,
-                                Optional.absent(),
-                                qm.dataMessage
+            List(paralellismFactor) {
+                Channel<QueuedMessage>(paralellismFactor).also { chan ->
+                    app.coroutineScope.launch(IO) {
+                        while (!chan.isClosedForReceive) {
+                            val qm = chan.receive()
+                            qm.result.complete(
+                                messageSenderOf(qm.sender).sendMessage(
+                                    qm.recipient,
+                                    Optional.absent(),
+                                    qm.dataMessage
+                                )
                             )
-                        )
+                        }
                     }
                 }
             }
-        }
+        }.random()
 
 
 
-// FAILED BATCHING ATTEMPTS (both not faster and get stuck when mesages are not increments of QUEUE_SIZE)
+/****************** FAILED THROTTLING ATTEMPTS ************************
+ * these only ever send one at a time!
+ */
+//    private val messageQueues = ConcurrentHashMap<String,SendChannel<QueuedMessage>>()
+//    private suspend fun messageQueueOf(account: VerifiedAccount): SendChannel<QueuedMessage> =
+//        getMemoized(messageQueues, account.username) {
+//              ******* Actor way *******
+////            app.coroutineScope.actor<QueuedMessage>(IO, MESSAGE_QUEUE_SIZE) {
+////                for (qm in channel) {
+////                    qm.result.complete(
+////                        messageSenderOf(qm.sender).sendMessage(
+////                            qm.recipient,
+////                            Optional.absent(),
+////                            qm.dataMessage
+////                        )
+////                    )
+////                }
+////            }
+////        }
+//            **** Channel way *****
+//            Channel<QueuedMessage>(MESSAGE_QUEUE_SIZE).also {
+//                app.coroutineScope.launch(IO) {
+//                    while(!it.isClosedForReceive) {
+//                        val qm = it.receive()
+//                        qm.result.complete(
+//                            messageSenderOf(qm.sender).sendMessage(
+//                                qm.recipient,
+//                                Optional.absent(),
+//                                qm.dataMessage
+//                            )
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//
+
+
+/*****************FAILED BATCHING ATTEMPTS
+ *both not faster and get stuck when mesages are not increments of QUEUE_SIZE)
+ */
+//    private val messageQueues = ConcurrentHashMap<String,SendChannel<QueuedMessage>>()
 //    private suspend fun messageQueueOf(account: VerifiedAccount): Channel<QueuedMessage> =
 //        getMemoized(messageQueues, account.username) {
 //            Channel<QueuedMessage>(MESSAGE_QUEUE_SIZE).also { chan ->
@@ -147,5 +178,12 @@ class SignalSender(private val app: Application) {
         val result = CompletableDeferred<SendMessageResult>()
         messageQueueOf(sender).send(QueuedMessage(sender, recipient, dataMessage, result))
         return result.await()
+
+        /*** FULLY PARALLELIZED VERSION ***/
+//        return messageSenderOf(sender).sendMessage(
+//            recipient,
+//            Optional.absent(),
+//            dataMessage
+//        )
     }
 }
