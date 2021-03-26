0902b95e5c36a6b356afdfacac370c8ba1400754
aguestuser <aguestuser@riseup.net>
2021-03-26 11:21:16 -0400

    [201] refactor LibSignalLogger

--- signalc/src/main/kotlin/info/signalboost/signalc/logging/LibSignalLogger.kt
+++ signalc/src/main/kotlin/info/signalboost/signalc/logging/LibSignalLogger.kt
@@ -6,16 +6,20 @@ import org.whispersystems.libsignal.logging.SignalProtocolLoggerProvider
 
 object LibSignalLogger: SignalProtocolLogger, KLoggable {
     override val logger = logger()
+    private val excludedTags = setOf("WebSocket", "WebSocketConnection")
+
     fun init() = SignalProtocolLoggerProvider.setProvider(LibSignalLogger)
 
     override fun log(priority: Int, tag: String, message: String) {
-        val logMessage = "[$tag]: $message"
-        when (priority) {
-            SignalProtocolLogger.VERBOSE -> logger.trace(logMessage)
-            SignalProtocolLogger.DEBUG -> logger.debug(logMessage)
-            SignalProtocolLogger.INFO -> logger.info(logMessage)
-            SignalProtocolLogger.WARN -> logger.warn(logMessage)
-            SignalProtocolLogger.ERROR, SignalProtocolLogger.ASSERT -> logger.error(logMessage)
+        if (excludedTags.contains(tag)) return
+        "[$tag]: $message".let {
+            when (priority) {
+                SignalProtocolLogger.VERBOSE -> logger.trace(it)
+                SignalProtocolLogger.DEBUG -> logger.debug(it)
+                SignalProtocolLogger.INFO -> logger.info(it)
+                SignalProtocolLogger.WARN -> logger.warn(it)
+                SignalProtocolLogger.ERROR, SignalProtocolLogger.ASSERT -> logger.error(it)
+            }
         }
     }
 }
