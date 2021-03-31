a0f7fad5513a09d53cafe53473de4ab0e33bc4d3
aguestuser <aguestuser@riseup.net>
2021-03-31 15:56:17 -0400

    [415] extract load test helpers into `simulator.helpers`
    - testSendingN, sdMessageOf, and print live in helpers
    - testSendingN returns a report
    - calling test prints the report
    - `numBots` is now private, so that different tests may set different
    - values for `numRecipients`, but we always subscribe to
      the full `numBots` (to avoid situation in which we want to run a
      "hot" test that is trying to send to numbers to which the receiver
      has not subscribed)

--- simulator/constants.js
+++ simulator/constants.js
@@ -1,22 +1,21 @@
 const { range } = require('lodash')
 
-const numBots = 100
-const numChannels = 4
-const channelSizes = [1, 10, 100, 1000]
+// constants
+const numBots = 1000
+const numChannels = 10
 
 const firstBotNumber = 10000000000
 const firstSignalcNumber = 20000000000
 const firstSignaldNumber = 23000000000
 
 const format = number => `+${number}`
+
+// exports
 const botPhoneNumbers = range(firstBotNumber, firstBotNumber + numBots).map(format)
 const signalcPhoneNumbers = range(firstSignalcNumber, firstSignalcNumber + numChannels).map(format)
 const signaldPhoneNumbers = range(firstSignaldNumber, firstSignaldNumber + numChannels).map(format)
 
 module.exports = {
-  numBots,
-  numChannels,
-  channelSizes,
   botPhoneNumbers,
   signalcPhoneNumbers,
   signaldPhoneNumbers,
--- simulator/receive.js
+++ simulator/receive.js
@@ -4,6 +4,10 @@ const { take } = require('lodash')
 const { loggerOf } = require('../app/util')
 const logger = loggerOf('receive')
 
+// NOTE: it is better for repeat runs if we always register the receiver
+// to subscribe to the total number of bots we have, rather than whatever
+// subset is currently under test.
+
 ;(async () => {
   const botsUnderTest = take(botPhoneNumbers, numBots)
   logger.log(`Starting receiver with ${numBots} bots...`)
