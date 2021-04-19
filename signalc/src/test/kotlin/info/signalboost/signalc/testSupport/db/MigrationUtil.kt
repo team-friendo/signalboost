0b2521648a9cf9bf5bc32cd4fe95b5bdfd826ebd
aguestuser <aguestuser@riseup.net>
2021-04-19 13:40:22 -0400

    [473] udpate `db.testUtil.MigrationUtil`
    - provide standalone file for generating schema info for migrations
    - and document how to use it!

--- signalc/src/test/kotlin/info/signalboost/signalc/testSupport/db/MigrationUtil.kt
+++ signalc/src/test/kotlin/info/signalboost/signalc/testSupport/db/MigrationUtil.kt
@@ -5,22 +5,27 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 
-/***
- * UTILITY FOR PRINTING SCHEMA FOR GENERATING MIGRATIONS
+/***********************************************************
+ * UTILITY TO PRINT DB SCHEMA FOR USE IN LIQUIBASE MIGRATIONS
  *
  * Usage:
  * - create or edit model code in `db.models`
- * - change `table` to model you just changed
- * - find sql you need in stdout output
- * - add sql to `migrations/changeset.sql`
- * - (including username/millis since epoc / options & teardown in commens)
+ *   - consult docs for DSL: https://github.com/JetBrains/Exposed/wiki/DSL
+ * - change `table` below to match classname of model you just changed
+ * - run this file in intellij, find sql you need in `Run` window output
+ * - add sql to `migrations/changelog.postgresql.sql`
+ * - also add appropriate comments in metadata:
+ *   - prefix comment to include in line above each sql statement you want to consider a "change:
+ *     `-- changeset <username><millis-since-epoch><nth-migration-run-at-that-time> failOnError:fase`
+ *   - postfix comment to include in line below each change:
+ *     `-- rollback <sql to execute to rollback the sql below prefix comment>`
  * - run `gradle update` (or `make sc.db.migrate`)
- * - use psql shell to make sure it worked! (`make sc.psql`)
+ * - use psql shell to make sure it worked! (`make sc.db.psql`)
  *
  * Prereq:
  * - must have a `signalc_scratch` db in local postgres container
  * - psql -q postgresql://postgres@localhost:5432 -U postgres -c "create database signalc_scratch;"
- ***/
+ ************************************************************/
 
 fun main() {
     // change this assignment to
