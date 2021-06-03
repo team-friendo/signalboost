5641256ae08438aad0796783c96c740ca204bdef
aguestuser <aguestuser@riseup.net>
2021-06-02 22:36:37 -0400

    [493] remove length restriction on session records

--- signalc/src/main/kotlin/info/signalboost/signalc/db/Sessions.kt
+++ signalc/src/main/kotlin/info/signalboost/signalc/db/Sessions.kt
@@ -5,12 +5,10 @@ import org.jetbrains.exposed.sql.Table
 
 
 object Sessions: Table(), ContactRecord, DeviceRecord  {
-    private const val SESSION_BYTE_ARRAY_LENGTH = 32
-
     override val accountId = varchar("account_id", 255)
     override val contactId = varchar("contact_id", 255)
     override val deviceId = integer("device_id")
-    val sessionBytes = binary("session_bytes", SESSION_BYTE_ARRAY_LENGTH)
+    val sessionBytes = binary("session_bytes")
 
     override val primaryKey = PrimaryKey(accountId, contactId, deviceId)
 }
--- signalc/src/main/kotlin/info/signalboost/signalc/store/protocol/SignalcSessionStore.kt
+++ signalc/src/main/kotlin/info/signalboost/signalc/store/protocol/SignalcSessionStore.kt
@@ -1,12 +1,10 @@
 package info.signalboost.signalc.store.protocol
 
 import info.signalboost.signalc.db.*
-import info.signalboost.signalc.db.ContactRecord.Companion.findByContactId
 import info.signalboost.signalc.db.ContactRecord.Companion.findManyByContactId
 import info.signalboost.signalc.db.DeviceRecord.Companion.deleteByAddress
 import info.signalboost.signalc.db.DeviceRecord.Companion.findByAddress
 import info.signalboost.signalc.db.DeviceRecord.Companion.updateByAddress
-import info.signalboost.signalc.db.Sessions.deviceId
 import info.signalboost.signalc.db.Sessions.sessionBytes
 import org.jetbrains.exposed.sql.*
 import org.whispersystems.libsignal.SignalProtocolAddress
